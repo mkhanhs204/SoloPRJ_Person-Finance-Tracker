c9947868d19cdb1cad06ba6ab691eb5b02a72ecc
Ho Minh Khanh <mkhhanhs204@gmail.com>
2026-07-17 07:59:33 +0700

    +

---
+++ SoloPRJ_Personal Finance Tracker/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace SoloPRJ_Personal_Finance_Tracker
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
